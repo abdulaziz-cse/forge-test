6daf8a50335e15b72fe94702af731a27f05dcce7
Abdulaziz <31155566+abdulazoz@users.noreply.github.com>
2019-05-15 00:12:30 +0300

    all website

--- resources/views/send.blade.php
+++ resources/views/send.blade.php
@@ -37,7 +37,33 @@
                                                           <th scope="row">{{ $send->id }}</th>
                                                           <td>{{ $send->name }}</td>
                                                           <td>{{ $send->email }}</td>
-                                                          <td>{{ $send->message }}</td>
+                                                          <td>
+                                                            <!-- Button trigger modal -->
+                                                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+
+                                                                المقترح
+                                                            </button>
+                                                            <!-- Modal -->
+                                                            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                                              <div class="modal-dialog modal-dialog-centered" role="document">
+                                                                <div class="modal-content">
+                                                                  <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                      <span aria-hidden="true">&times;</span>
+                                                                    </button>
+                                                                  </div>
+                                                                  <div class="modal-body">
+                                                                        {{ $send->message }}
+                                                                  </div>
+                                                                  <div class="modal-footer">
+                                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">إغلاق</button>
+                                                                  </div>
+                                                                </div>
+                                                              </div>
+                                                            </div>
+
+                                                          </td>
 
 
                                                         </tr>
